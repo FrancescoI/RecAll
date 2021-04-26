d845e550bab4ac405ffcc2fcce1eed820e632a02
Francesco Imbriglia <francesco.imbriglia01@gmail.com>
2021-04-26 13:07:54 +0200

    first commit

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
-ECHO is on.
+from setuptools import find_packages, setup
+
+
+# Import version
+#__builtins__.__SPOTLIGHT_SETUP__ = True
+from src import __version__ as version  # NOQA
+
+
+setup(
+    name='recall',
+    version=version,
+    packages=find_packages(),
+    install_requires=['torch', 'pandas', 'scipy', 'numpy', 'sklearn'],
+    license='MIT',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: MIT License',
+                 'Topic :: Scientific/Engineering :: Artificial Intelligence'],
+)
