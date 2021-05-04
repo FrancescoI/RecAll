1b21f067e475bbfc6f3fb29d22a5703d035750de
sciaccale <alessio.sciacchitano@gmail.com>
2021-05-04 10:39:01 +0200

    my example

--- example.py
+++ example.py
@@ -1,43 +1,48 @@
 # -*- coding: utf-8 -*-
 
-# from model.recall import *
-# from model.dataset.dataset import *
-
-# missoni = MyDataset(brand='missoni')
-
-# missoni.fit(metadata=['saleLine', 'macro'])
-
-# recall = Recall(dataset=missoni, 
-#                 n_factors=80, 
-#                 net_type='lightfm', 
-#                 use_metadata=True, 
-#                 use_cuda=False)
-
-# optimizer = torch.optim.Adam(recall.net.parameters(),
-#                              lr=3e-3)
-
-# recall.fit(optimizer=optimizer,
-#            batch_size=10_240,
-#            epochs=20,
-#            split_train_test=True,
-#            verbose=True)
-
+from model.recall import *
 from model.dataset.dataset import *
-import pandas as pd
-import numpy as np
 
-df = pd.DataFrame(data=np.arange(0,50).reshape(10,5),columns=['A','B','C', 'metadata_1', 'metadata_2'])
+missoni = pd.read_csv('missoni.csv').sample(n = 1000)
 
-mydataset = Dataset(df['A'].values,
-                    df['B'].values
-                    #weights=df['C'].values,
-                    #metadata=df[['metadata_1', 'metadata_2']].values,
-                    #metadata_name =['metadata_1','metadata_2']
+
+dataset = Dataset(missoni['hashedEmail'].values,
+                    missoni['product'].values,
+                    metadata=missoni[['saleLine', 'gender','macro']].values,
+                    metadata_name=['saleline','gender','macro']
                     )
 
+recall = Recall(dataset, 
+                80,
+                net_type = 'hybrid_cf', 
+                use_metadata = True,
+                use_cuda=False, 
+                verbose = True)
 
-from torch.utils.data import DataLoader
-data_loader = CustomDataLoader(dataset=mydataset, batch_size=1)
+optimizer = torch.optim.Adam(recall.net.parameters(),
+                             lr=3e-3)
 
-for i, k in enumerate(data_loader):
-    print(k)
+recall.fit(optimizer=optimizer,
+           batch_size=128,
+           epochs=20,
+           splitting_train_test=True)
+
+# from model.dataset.dataset import *
+# import pandas as pd
+# import numpy as np
+
+# df = pd.DataFrame(data=np.arange(0,50).reshape(10,5),columns=['A','B','C', 'metadata_1', 'metadata_2'])
+
+# mydataset = Dataset(df['A'].values,
+#                     df['B'].values
+#                     #weights=df['C'].values,
+#                     #metadata=df[['metadata_1', 'metadata_2']].values,
+#                     #metadata_name =['metadata_1','metadata_2']
+#                     )
+
+
+# from torch.utils.data import DataLoader
+# data_loader = CustomDataLoader(dataset=mydataset, batch_size=1)
+
+# for i, k in enumerate(data_loader):
+#     print(k)
